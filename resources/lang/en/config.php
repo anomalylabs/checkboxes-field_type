b60297e1fccaa750cfda22012e17e6079443c160
Ryan Thompson <service@aiwebsystems.com>
2015-05-13 23:43:45 -0500

    Config lang

--- resources/lang/en/config.php
+++ resources/lang/en/config.php
@@ -2,6 +2,8 @@
 
 return [
     'options' => [
-        'label' => 'Options'
+        'label'        => 'Checkboxes Options',
+        'instructions' => 'Enter options below in <strong>key: Value</strong> format. Enter each option on a new line.',
+        'placeholder'  => "foo: Foo\nbar: Bar"
     ]
 ];
