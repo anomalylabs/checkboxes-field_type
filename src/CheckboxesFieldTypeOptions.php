ba61448d23dfaf96c225b543640bc025b55548ea
Ryan Thompson <service@aiwebsystems.com>
2015-12-02 01:07:42 -0600

    Allow passing collections to options

--- src/CheckboxesFieldTypeOptions.php
+++ src/CheckboxesFieldTypeOptions.php
@@ -1,7 +1,9 @@
 <?php namespace Anomaly\CheckboxesFieldType;
 
 use Anomaly\CheckboxesFieldType\Command\ParseOptions;
+use Anomaly\Streams\Platform\Entry\Contract\EntryInterface;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Support\Collection;
 
 /**
  * Class CheckboxesFieldTypeOptions
@@ -27,6 +29,25 @@ class CheckboxesFieldTypeOptions
 
         if (is_string($options)) {
             $options = $this->dispatch(new ParseOptions($options));
+        }
+
+        if ($options instanceof Collection && $options->isEmpty()) {
+            $options = [];
+        }
+
+        if ($options instanceof Collection && is_object($first = $options->first())) {
+
+            if ($first instanceof EntryInterface) {
+                $value = $first->getTitleName();
+            } else {
+                $value = 'id';
+            }
+
+            $options = $options->lists($value, 'id');
+        }
+
+        if ($options instanceof Collection && is_string($options->first())) {
+            $options = $options->all();
         }
 
         $fieldType->setOptions($options);
