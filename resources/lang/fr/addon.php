505477684f98e3f1051b6a651a34b284863ce33f
MarceauKa <marceau@casals.fr>
2016-03-02 16:08:45 +0100

    Update the French localization

--- resources/lang/fr/addon.php
+++ resources/lang/fr/addon.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    'name'        => 'Cases à cocher',
-    'description' => 'Type de champs pour les cases à cocher.'
+    'title'       => 'Cases à cocher',
+    'name'        => 'Type de champs Cases à cocher',
+    'description' => 'Type de champs pour utiliser des cases à cocher.'
 ];
