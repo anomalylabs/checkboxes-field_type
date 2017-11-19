e5e836bc39a69fecc7b7b9b0ecd5e09f1cc50a76
Denis Efremov <efremov.a.denis@gmail.com>
2017-11-19 07:45:14 +0300

    Update tags.js

--- resources/js/tags.js
+++ resources/js/tags.js
@@ -1,26 +1,25 @@
 (function (window, document) {
 
-    let fields = Array.from(
-        document.querySelectorAll('select[data-provides="anomaly.field_type.checkboxes"]')
-    );
+  let fields = Array.from(
+    document.querySelectorAll('select[data-provides="anomaly.field_type.checkboxes"]')
+  );
 
-    fields.forEach(function (field) {
+  fields.forEach(function (field) {
 
-        let choices = new Choices(field, {
-            removeItemButton: true,
-        });
-
-        /**
-         * Fix an issue where when all options
-         * are removed the last option element
-         * is not removed from the DOM.
-         */
-        field.addEventListener('removeItem', function (event) {
-
-            if (choices.getValue().length == 0) {
-                event.target.removeChild(event.target.options[0])
-            }
-
-        }, false);
+    let choices = new Choices(field, {
+      removeItemButton: true,
     });
+
+    field.addEventListener('removeItem', function (id, value, label, groupValue) {
+
+      if (!value) {
+        field.options.forEach(function (option) {
+          field.removeChild(option);
+        });
+      }
+        
+    });
+
+  });
+
 })(window, document);
