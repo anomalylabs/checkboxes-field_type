08deaf768da3c5a4d1bf2b5c63ef7598cdbe60e3
Ryan Thompson <service@aiwebsystems.com>
2019-11-21 09:47:04 -0600

    Checkboxes

---
+++ resources/views/checkboxes.blade.php
@@ -0,0 +1,43 @@
+<div class="custom-inputs-stacked">
+
+    @foreach ($fieldType->getOptions() as $key => $value)
+        @if (is_array($value))
+            <p>
+                <strong>{{ $key }}</strong>
+
+                @foreach ($value as $key => $value)
+                <label class="{#custom-control#} custom-checkbox" style="display: block;">
+                    <input
+                            type="checkbox"
+                            value="{{ $value }}"
+                            name="{{ $fieldType->getInputName() }}[]"
+                            {{-- {!! html_attributes($fieldType->getAttributes()) !!} --}}
+                            {{-- {{ $fieldType->getReadonly() ? 'onclick="return false;"' }} --}}
+                            {{-- {{ value in field_type.config.disabled ? 'disabled' }} --}}
+                            {{-- {{ value in field_type.config.checked ? 'checked' }} --}}
+                            {{-- {{ value in field_type.value ? 'checked' }} --}}
+                            >
+
+                    <span class="{#custom-control-indicator#}"></span>
+
+                    {!! $key !!}
+                </label>
+                @endforeach
+            </p>
+        @else
+            <label class="{#custom-control#} custom-checkbox" style="display: block;">
+                <input
+                        type="checkbox"
+                        value="{{ $value }}"
+                        name="{{ $fieldType->getInputName() }}[]"
+                        {{-- {!! html_attributes($fieldType->getAttributes()) !!} --}}
+                        {{-- {{ value in field_type.config.disabled ? 'disabled' }}
+                        {{ value in field_type.value ? 'checked' }} --}}
+                        >
+
+                <span class="{#custom-control-indicator#}">{!! $$key !!}</span>
+            </label>
+        @endif
+    @endforeach
+
+</div>
